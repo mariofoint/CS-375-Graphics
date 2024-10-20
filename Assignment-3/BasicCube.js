983aea936e85169420fac5f31aeac204f8363b2d
mariofoint <111588802+mariofoint@users.noreply.github.com>
2024-10-20 15:03:54 -0700

    completed basicCube

--- Assignment-3/BasicCube.js
+++ Assignment-3/BasicCube.js
@@ -7,11 +7,99 @@
 
 class BasicCube {
     constructor(gl, vertexShader, fragmentShader) {
+        vertexShader = `
+            in vec4 aPosition;
+            in vec4 aColor;
 
-        // let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+            uniform mat4 P;
+            uniform mat4 MV;
 
+            out vec4 vColor;
+
+            void main() {
+                gl_Position = P * MV * aPosition;
+                vColor = aColor;
+            }
+        `;
+        fragmentShader ||= `
+        in vec4 vColor;
+        out vec4 fColor;
+
+        void main() {
+            const vec4 backColor = vec4(0,0,0,1);
+            fColor = gl_FrontFacing ? vColor : backColor;
+            //fColor = vColor;
+        }
+        `;
+
+        let program = new ShaderProgram(gl, this, vertexShader, fragmentShader);
+        
+
+
+        const positions = new Float32Array([
+            //z
+            -0.5, -0.5,  0.5,   0.5, -0.5,  0.5,   0.5,  0.5,  0.5,
+            -0.5, -0.5,  0.5,   0.5,  0.5,  0.5,  -0.5,  0.5,  0.5,
+        
+            //-z
+            -0.5, -0.5, -0.5,  -0.5,  0.5, -0.5,   0.5,  0.5, -0.5,
+            -0.5, -0.5, -0.5,   0.5,  0.5, -0.5,   0.5, -0.5, -0.5,
+        
+            //-x
+            -0.5, -0.5, -0.5,  -0.5, -0.5,  0.5,  -0.5,  0.5,  0.5,
+            -0.5, -0.5, -0.5,  -0.5,  0.5,  0.5,  -0.5,  0.5, -0.5,
+        
+            //x
+            0.5, -0.5, -0.5,   0.5,  0.5, -0.5,   0.5,  0.5,  0.5,
+            0.5, -0.5, -0.5,   0.5,  0.5,  0.5,   0.5, -0.5,  0.5,
+        
+            //y
+            -0.5,  0.5, -0.5,  -0.5,  0.5,  0.5,   0.5,  0.5,  0.5,
+            -0.5,  0.5, -0.5,   0.5,  0.5,  0.5,   0.5,  0.5, -0.5,
+        
+            //-y
+            -0.5, -0.5, -0.5,   0.5, -0.5, -0.5,   0.5, -0.5,  0.5,
+            -0.5, -0.5, -0.5,   0.5, -0.5,  0.5,  -0.5, -0.5,  0.5
+        ]);
+        
+
+        const colors = new Float32Array([            
+            1.0, 0.0, 0.0, 1.0,  1.0, 0.0, 0.0, 1.0,  1.0, 0.0, 0.0, 1.0,
+            1.0, 0.0, 0.0, 1.0,  1.0, 0.0, 0.0, 1.0,  1.0, 0.0, 0.0, 1.0,
+            
+            0.0, 1.0, 0.0, 1.0,  0.0, 1.0, 0.0, 1.0,  0.0, 1.0, 0.0, 1.0,
+            0.0, 1.0, 0.0, 1.0,  0.0, 1.0, 0.0, 1.0,  0.0, 1.0, 0.0, 1.0,
+            
+            0.0, 0.0, 1.0, 1.0,  0.0, 0.0, 1.0, 1.0,  0.0, 0.0, 1.0, 1.0,
+            0.0, 0.0, 1.0, 1.0,  0.0, 0.0, 1.0, 1.0,  0.0, 0.0, 1.0, 1.0,
+            
+            1.0, 1.0, 0.0, 1.0,  1.0, 1.0, 0.0, 1.0,  1.0, 1.0, 0.0, 1.0,
+            1.0, 1.0, 0.0, 1.0,  1.0, 1.0, 0.0, 1.0,  1.0, 1.0, 0.0, 1.0,
+            
+            0.0, 1.0, 1.0, 1.0,  0.0, 1.0, 1.0, 1.0,  0.0, 1.0, 1.0, 1.0,
+            0.0, 1.0, 1.0, 1.0,  0.0, 1.0, 1.0, 1.0,  0.0, 1.0, 1.0, 1.0,
+        
+            1.0, 0.0, 1.0, 1.0,  1.0, 0.0, 1.0, 1.0,  1.0, 0.0, 1.0, 1.0,
+            1.0, 0.0, 1.0, 1.0,  1.0, 0.0, 1.0, 1.0,  1.0, 0.0, 1.0, 1.0
+        ]);
+
+        this.P = mat4();
+        this.MV = mat4();
+
+        let aPosition = new Attribute(gl, program, "aPosition", positions, 3, gl.FLOAT);
+        let aColor = new Attribute(gl, program, "aColor", colors, 4, gl.FLOAT);
+       
+        
         this.draw = () => {
-            // program.use();
+            //this.MV = translate(-0.5, -0.5, -0.5); i tried this this got messed up so i just converted the values
+             program.use();
+             aPosition.enable();
+             aColor.enable();
+
+             gl.drawArrays(gl.TRIANGLES, 0, positions.length / 3);
+
+             aPosition.disable();
+             aColor.disable();
         };
     }
 };
