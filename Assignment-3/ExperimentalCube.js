b59f98a1595ba4532b3b720289a03adfcccadd9a
mariofoint <111588802+mariofoint@users.noreply.github.com>
2024-10-21 14:48:05 -0700

    use buffers instead of Indices and Attribute Helpers class

--- Assignment-3/ExperimentalCube.js
+++ Assignment-3/ExperimentalCube.js
@@ -74,28 +74,52 @@ class ExperimentalCube {
         this.P = mat4();
         this.MV = mat4();
 
-        this.indices1 = new Indices(gl, indices1);
-        //this.indices2 = new Indices(gl, indices2);
-        let aPosition = new Attribute(gl, program, "aPosition", positions, 3, gl.FLOAT);
-        let aColor = new Attribute(gl, program, "aColor", colors, 4, gl.FLOAT);
+        let indicesBuff = gl.createBuffer(); //new Indices(gl, indices1);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuff);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices1, gl.STATIC_DRAW);
 
+        //this.indices2 = new Indices(gl, indices2);
+
+        let aPosition = gl.createBuffer(); //new Attribute(gl, program, "aPosition", positions, 3, gl.FLOAT);
+        gl.bindBuffer(gl.ARRAY_BUFFER, aPosition);
+        gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+
+        let aColor = gl.createBuffer();//new Attribute(gl, program, "aColor", colors, 4, gl.FLOAT);
+        gl.bindBuffer(gl.ARRAY_BUFFER, aColor);
+        gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
 
         this.draw = () => {
             //this.MV = translate(-0.5, -0.5, -0.5);
             program.use();
-            aPosition.enable();
-            aColor.enable();
 
-            this.indices1.enable();
-            gl.drawElements(gl.TRIANGLE_STRIP, this.indices1.count, this.indices1.type, 0);
-            this.indices1.disable();
+            //aPosition.enable();
+            let aPositionIndex = gl.getAttribLocation(program.program, "aPosition");
+            gl.bindBuffer(gl.ARRAY_BUFFER, aPosition);
+            gl.vertexAttribPointer(aPositionIndex, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(aPositionIndex);
+
+            //aColor.enable();
+            let aColorIndex = gl.getAttribLocation(program.program, "aColor");
+            gl.bindBuffer(gl.ARRAY_BUFFER, aColor);
+            gl.vertexAttribPointer(aColorIndex, 4, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(aColorIndex);
+
+            //this.indices1.enable();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuff);
+            //gl.drawElements(gl.TRIANGLE_STRIP, this.indices1.count, this.indices1.type, 0);
+            gl.drawElements(gl.TRIANGLE_STRIP, 14, gl.UNSIGNED_BYTE, 0);
+            //this.indices1.disable();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
 
            // this.indices2.enable();
             //gl.drawElements(gl.TRIANGLE_STRIP, this.indices2.count, this.indices2.type, 0);
            // this.indices2.disable();
 
-            aPosition.disable();
-            aColor.disable();
+            //aPosition.disable();
+            //aColor.disable();
+            gl.disableVertexAttribArray(aPositionIndex);
+            gl.disableVertexAttribArray(aColorIndex);
+            gl.bindBuffer(gl.ARRAY_BUFFER, null);
         };
     }
 };
